cf30dc409b00910a2738e879cf0ededd5a5ff4ad
leocareer <leo.ph.career@gmail.com>
2024-02-19 17:46:39 +0100

    angular, web app, pages: feedback, product, shop. ready for deploy

---
+++ src/app/pages/product/product.component.ts
@@ -0,0 +1,12 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-product',
+  standalone: true,
+  imports: [CommonModule],
+  template: `product`,
+})
+export class ProductComponent {
+
+}
---
+++ src/app/pages/shop/shop.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-shop',
+  standalone: true,
+  imports: [CommonModule],
+  template: `<h1>main shop</h1>`,
+})
+export class ShopComponent {
+
+}
